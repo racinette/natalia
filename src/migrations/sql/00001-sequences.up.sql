63809f3482074f88a12beffc2449c97974518e24
Andrei Karavatski <witaju@172.31.57.49-vpn.dhcp.yndx.net>
2026-04-20 20:32:58 +0300

    removed migrations, added to buffer

--- src/migrations/sql/00001-sequences.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE SEQUENCE {{schema}}.executor_id_seq
-    START 0
-    MINVALUE 0
-    MAXVALUE 2147483647
-    CACHE 100
-    CYCLE;
-
-CREATE SEQUENCE {{schema}}.channel_message_seq_num_seq;
